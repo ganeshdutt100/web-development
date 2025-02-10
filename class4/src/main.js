a2306e7fff399822fd97e0723af898cb3d66e854
Ganesh Dutt <gdutt2304@gmail.com>
2025-02-10 12:52:07 +0530

    Node

--- class4/src/main.js
+++ class4/src/main.js
@@ -3,3 +3,13 @@ var typed = new Typed("#element", {
   typeSpeed: 100,
   loop: true,
 });
+
+const mobileMenuIcon = document.getElementById("mobileMenuIcon");
+const mobileNav = document.getElementById("mobileNav");
+
+// Add a click event listener to the mobile menu icon
+mobileMenuIcon.addEventListener("click", () => {
+  // Toggle the visibility of the mobile navigation menu
+  mobileNav.classList.toggle("hidden");
+  mobileNav.classList.add("transitionNav");
+});
